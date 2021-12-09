50a0053696b97453e60b118892cbc273ba767c35
Juan Pedro Developer <juanpedro.developer@gmail.com>
2021-12-09 00:47:14 -0300

    primer commitç

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const inputCalc = document.getElementById("display__current")
+let calculate = "";
+
+const press = (num)=>{
+    calculate += num;
+    document.getElementById("display__current").innerHTML = calculate
+}
+
+const equal = ()=>{
+    document.getElementById("display__log").innerHTML = `${calculate} = `;
+    document.getElementById("display__current").innerHTML = eval(calculate);
+}
+
+const eraseAll = ()=>{
+    calculate="";
+    document.getElementById("display__log").innerHTML = "Calcular";
+    document.getElementById("display__current").innerHTML = 0;
+}
+const erase = ()=>{
+    calculate = calculate.slice(0,-1);
+    document.getElementById("display__log").innerHTML = `${calculate} = `;
+    document.getElementById("display__current").innerHTML = calculate;
+}
