7cf00dc9bbae495d354de28b7b578127d9476822
Juan Pedro <juanpedro.developer@gmail.com>
2021-12-22 20:16:35 -0300

    add keyboard

--- app/app.js
+++ app/app.js
@@ -1,26 +1,53 @@
 let calculate = "";
 
-const press = (num)=>{
-    calculate += num;
-    document.getElementById("display__current").innerHTML = calculate
+const displayTop = document.getElementById('display__log');
+const displayBot = document.getElementById('display__current')
+const ac = document.querySelector('.btn__ac');
+const erase = document.querySelector('.btn__erase');
+const equal = document.querySelector('.btn__equal');
+
+const resolve = ()=> {
+    displayTop.textContent = `${calculate} = `;
+    displayBot.innerHTML = eval(calculate)
+}
+const deleteAll = () =>{
+    calculate ="";
+    displayTop.textContent = "Calcular";
+    displayBot.textContent = "0";
 }
 
-const equal = ()=>{
-    document.getElementById("display__log").innerHTML = `${calculate} = `;
-    document.getElementById("display__current").innerHTML = eval(calculate);
-}
-console.log("gola");
-
-const eraseAll = ()=>{
-    calculate="";
-    document.getElementById("display__log").innerHTML = "Calcular";
-    document.getElementById("display__current").innerHTML = 0;
-}
-
-const erase = ()=>{
+const deleteOne = () => {
     calculate = calculate.slice(0,-1);
-    document.getElementById("display__log").innerHTML = `${calculate} = `;
-    document.getElementById("display__current").innerHTML = calculate;
+    // displayTop.textContent = `${calculate} = `;
+    (calculate.length == 0) ? calculate = "0" : false;
+    displayBot.textContent = calculate;
 }
 
+ac.addEventListener("click", ()=> deleteAll())
+erase.addEventListener("click", ()=> deleteOne())
+equal.addEventListener("click", ()=> resolve())
+
+
+const press = (num)=>{
+    (calculate == 0) ? calculate ="": false;
+    calculate += num;
+    displayBot.textContent = calculate
+}
+
+const arrayKey = ["0","1","2","3","4","5","6","7","8","9","/","*","+","-"];
+
+
+window.addEventListener("keydown",(event)=>{
+    console.log(event)
+    arrayKey.includes(event.key)
+    ? press(event.key)
+    : event.key == "Enter"
+    ? resolve()
+    : event.code == "Backspace"
+    ? deleteOne()
+    : event.code == "Delete"
+    ? deleteAll()
+    : false
+
+})
 
