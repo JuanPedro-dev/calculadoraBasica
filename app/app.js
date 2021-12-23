dbd6c204fe76571734b318108a69fc7b80d845ef
Juan Pedro Developer <juanpedro.developer@gmail.com>
2021-12-23 15:42:55 -0300

    evitamos malas practicas

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
-let calculate = "";
+let calculate = ""; // inicializar contador
+const arrayKey = ["0","1","2","3","4","5","6","7","8","9","/","*","+","-","(",")"]; // filtrar valores del teclado
 
 const displayTop = document.getElementById('display__log');
 const displayBot = document.getElementById('display__current')
 const ac = document.querySelector('.btn__ac');
 const erase = document.querySelector('.btn__erase');
 const equal = document.querySelector('.btn__equal');
+const teclado = document.querySelector('.container__buttons');
 
+
+// funciones
 const resolve = ()=> {
     displayTop.textContent = `${calculate} = `;
     displayBot.innerHTML = eval(calculate)
@@ -15,30 +19,19 @@ const deleteAll = () =>{
     displayTop.textContent = "Calcular";
     displayBot.textContent = "0";
 }
-
 const deleteOne = () => {
     calculate = calculate.slice(0,-1);
     // displayTop.textContent = `${calculate} = `;
     (calculate.length == 0) ? calculate = "0" : false;
     displayBot.textContent = calculate;
 }
-
-ac.addEventListener("click", ()=> deleteAll())
-erase.addEventListener("click", ()=> deleteOne())
-equal.addEventListener("click", ()=> resolve())
-
-
 const press = (num)=>{
     (calculate == 0) ? calculate ="": false;
     calculate += num;
     displayBot.textContent = calculate
 }
-
-const arrayKey = ["0","1","2","3","4","5","6","7","8","9","/","*","+","-","(",")"];
-
-
+// Event Listener teclado 
 window.addEventListener("keydown",(event)=>{
-    console.log(event)
     arrayKey.includes(event.key)
     ? press(event.key)
     : event.key == "Enter"
@@ -48,6 +41,17 @@ window.addEventListener("keydown",(event)=>{
     : event.code == "Delete"
     ? deleteAll()
     : false
-
 })
+
+// Event Listener mouse o tactil
+ac.addEventListener("click", ()=> deleteAll())
+erase.addEventListener("click", ()=> deleteOne())
+equal.addEventListener("click", ()=> resolve())
+teclado.addEventListener("click", (event)=>{
+    if (arrayKey.includes(event.target.textContent)){
+        console.log(event.target.textContent)
+        press(event.target.textContent)
+    }
+})
+
 
